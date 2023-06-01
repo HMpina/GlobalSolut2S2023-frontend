d010f91fffaf78608a5f89fe6affbe686c3fb8c3
HMpina <henrique.marques.pina@gmail.com>
2023-06-01 19:24:07 -0300

    downgrade da versão do react-router-dom

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,22 +1,20 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
-
+import {Switch, Route} from 'react-router-dom'
+ 
 import Home from './pages/Home';
 import Hints from './pages/Hints';
 import HintsForm from './pages/Hints/Form';
 import HintsDetail from './pages/Hints/Detail';
-
-const Apps: React.FC = () => {
-  return (
-    <BrowserRouter>
-      <Route path="/" component={Home} />
-      <Route path="/dicas" component={Hints} />
-      <Route path="/dicas_cadastro" component={HintsForm} />
-      <Route path="/dicas_cadastro/:id" component={HintsForm} />
-      <Route path="/dicas/:id" component={HintsDetail} />
-    </BrowserRouter>
-  );
-};
-
-
-export default Apps;
+ 
+const Routes: React.FC = () => {
+    return(
+        <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/dicas" exact component={Hints} />
+            <Route path="/dicas_cadastro" exact component={HintsForm} />
+            <Route path="/dicas_cadastro/:id" exact component={HintsForm} />
+            <Route path="/dicas/:id" exact component={HintsDetail} />
+        </Switch>
+    );
+}
+export default Routes;
